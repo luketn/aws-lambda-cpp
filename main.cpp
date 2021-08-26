585ceb84130dd7881e2666ba0f69172bd7b3c71b
Jett Thompson <jett.thompson.59@gmail.com>
2021-08-27 08:08:14 +1000

    made the c++ code get the aws lambda api and call it

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
+#include <cstdlib>
 #include <iostream>
+#include <string>
 #include "./httplib.h"
 
-int main () {
-    httplib::Client cli("https://www.google.com.au");
+int main()
+{
+    const char* rawLambdaApi = std::getenv("AWS_LAMBDA_RUNTIME_API");
+    if (rawLambdaApi)
+    {
+        std::cout << "Lambda api: " << rawLambdaApi << std::endl;
+    }
+    else
+    {
+        std::cerr << "Could not find lambda api variable" << std::endl;
+        std::abort();
+    }
+    std::string lambdaApi(rawLambdaApi);
+    httplib::Client cli("http://" + lambdaApi + "/2018-06-01/runtime/invocation/next");
 
     auto res = cli.Get("/");
 
-    if (res) {
+    if (res)
+    {
         std::cout << res->status << std::endl;
         std::cout << res->body << std::endl;
-    } else {
-        std::cout << "error: " << httplib::to_string(res.error()) << std::endl;
+    }
+    else
+    {
+        std::cerr << "error: " << httplib::to_string(res.error()) << std::endl;
     }
 }
